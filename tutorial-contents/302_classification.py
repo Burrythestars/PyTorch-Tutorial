0a5a8c258be30ee521f58885cebddd1bbacbd453
Burrythestars <1083386771@qq.com>
2018-08-19 15:17:37 +0800

    Update 302_classification.py

--- tutorial-contents/302_classification.py
+++ tutorial-contents/302_classification.py
@@ -7,7 +7,6 @@ torch: 0.4
 matplotlib
 """
 import torch
-from torch.autograd import Variable
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
@@ -21,8 +20,6 @@ x1 = torch.normal(-2*n_data, 1)     # class1 x data (tensor), shape=(100, 2)
 y1 = torch.ones(100)                # class1 y data (tensor), shape=(100, 1)
 x = torch.cat((x0, x1), 0).type(torch.FloatTensor)  # shape (200, 2) FloatTensor = 32-bit floating
 y = torch.cat((y0, y1), ).type(torch.LongTensor)    # shape (200,) LongTensor = 64-bit integer
-x = Variable(x)                     # FloatTensor to Variable
-y = Variable(y)                     # LongTensor to Variable
 
 # The code below is deprecated in Pytorch 0.4. Now, autograd directly supports tensors
 # x, y = Variable(x), Variable(y)
